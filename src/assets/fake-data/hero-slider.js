1ccef706f4c0e13e58dd8eea8c6ff35d7c92cc1f
linhleba <linhleba.it@gmail.com>
2022-04-19 19:20:39 +0700

    update logo, redesign

--- src/assets/fake-data/hero-slider.js
+++ src/assets/fake-data/hero-slider.js
@@ -4,28 +4,28 @@ const img3 = require('../images/slider/slide_3.png').default;
 
 const heroSliderData = [
   {
-    title: 'Polo nữ Pima cao cấp',
+    title: 'Cây Cam Ngọt Của Tôi.',
     description:
-      'Nhắc đến sự đẳng cấp là không thể không nhắc đến dòng vải pima. Nó tạo nên chất lượng tốt nhất cho bất kỳ sản phẩm thời trang nào. Sợi vải pima dài và dày hơn sợi cotton thông thường gấp đôi nhờ công nghệ dệt tân tiến. Điều đó làm cho kết cấu áo polo chắc chắn, bền chặt, hạn chế tối đa xù lông, mềm mượt, bền màu, vô cùng đảm bảo sức khoẻ người dùng',
+      'Được dịch từ tiếng Anh-My Sweet Orange Tree là một tiểu thuyết của José Mauro de Vasconcelos. Cuốn sách được xuất bản lần đầu tiên vào năm 1968 và được sử dụng cho các lớp học văn của các trường tiểu học ở Brazil. Nó cũng đã được dịch và xuất bản ở Mỹ và Châu Âu',
     img: img1,
     color: 'blue',
     path: '/catalog/ao-thun-dinosaur-01',
   },
   {
-    title: 'Polo Nữ Dáng Suông Modal',
+    title: 'Đời ngắn đừng ngủ dài',
     description:
-      'Polo nữ dáng suông Modal sử dụng công nghệ vải cao cấp thân thiện với môi trường sản xuất độc quyền chống co rút vải, áo polo nữ vải modal là sản phẩm thích hợp cho các bạn có môi trường làm việc năng động như hiện nay',
+      'Đời Ngắn Đừng Ngủ Dài bằng những lời chia sẻ thật ngắn gọn, dễ hiểu về những trải nghiệm và suy ngẫm trong đời, Robin Sharma tiếp tục phong ...',
     img: img2,
-    path: '/catalog/ao-thun-dinosaur-02',
-    color: 'pink',
+    path: '/catalog/6892e3ee-2345-46b7-9add-ce7c0f9b0414',
+    color: 'orange',
   },
   {
-    title: 'Polo Nữ Coolmax Lacoste',
+    title: 'Đi tìm lẽ sống',
     description:
-      'Mẫu áo polo nữ được làm bằng chất liệu coolmax đem lại cảm giác mát lạnh khi mặc. Thiết kế mẫu áo polo coolmax này có kiểu dáng cực kỳ thoải mái. Tạo sự gọn gàng hứa hẹn sẽ là mẫu áo polo vô cùng hot trong thời điểm sắp tới',
+      '“Đi tìm lẽ sống là một cuốn sách ghi lại những trải nghiệm của Victor Frankl trong trại tập trung Auschwitz. Nó chắc chắn sẽ giúp độc giả có cách nhìn mới về lý do mà mình tồn tại.',
     img: img3,
-    path: '/catalog/ao-thun-dinosaur-03',
-    color: 'orange',
+    path: '/catalog/0dbb51bb-944d-44ee-a917-f76f94d7f13b',
+    color: 'pink',
   },
 ];
 
