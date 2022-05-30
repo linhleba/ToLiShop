569a1f19ea7d4ee15c59b7ed91e922932281fc79
linhleba <linhleba.it@gmail.com>
2022-05-30 21:44:41 +0700

    update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -13,16 +13,55 @@ import apiCaller from '../utils/apiCaller';
 const Product = (props) => {
   const [product, setProducts] = useState(null);
   //   const product = productData.getProductBySlug(props.match.params.slug);
+
+  const [userId, setUserId] = useState(null);
   console.log('slug is', props.match.params.slug);
+
+  const [recommendProductIds, setRecommendProductIds] = useState(null);
+
+  const [recommendProducts, setRecommendProducts] = useState([]);
 
   useEffect(() => {
     apiCaller(`api/book/${props.match.params.slug}`, 'get').then((res) => {
       setProducts(res.data);
     });
-  }, []);
-  //   console.log(result);
 
-  const relatedProducts = productData.getProducts(8);
+    let profile = localStorage.getItem('profile');
+    let access_jwt_token = JSON.parse(profile)?.access_jwt_token;
+    apiCaller('api/account/info', 'get', null, {
+      authorization: access_jwt_token,
+    }).then((res) => {
+      // console.log('user id is:', res.data);
+      setUserId(res.data.user_id);
+    });
+  }, []);
+
+  useEffect(() => {
+    apiCaller(`api/MLData/${userId}`).then((res) => {
+      // console.log('data recommdnd:', res.data?.yourData?.bookIds);
+      setRecommendProductIds(res.data?.yourData?.bookIds);
+    });
+  }, [userId]);
+
+  useEffect(() => {
+    console.log('recommedn product:', recommendProductIds);
+    if (recommendProductIds) {
+      Promise.all(
+        recommendProductIds.map((item) => {
+          apiCaller(`api/book/${item}`, 'get').then((res) => {
+            console.log('gia tri cua recomend', res);
+            let dataProduct = res.data;
+            setRecommendProducts((prevData) => ({
+              ...prevData,
+              dataProduct,
+            }));
+          });
+        }),
+      );
+    }
+  }, [recommendProductIds]);
+
+  //   console.log(result);
 
   React.useEffect(() => {
     window.scrollTo(0, 0);
@@ -36,10 +75,10 @@ const Product = (props) => {
         </SectionBody>
       </Section>
       <Section>
-        <SectionTitle>Có thể bạn thích</SectionTitle>
+        <SectionTitle>Gợi ý riêng cho bạn</SectionTitle>
         <SectionBody>
-          {/* <Grid col={4} mdCol={2} smCol={1} gap={20}>
-            {relatedProducts.map((item, index) => (
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {recommendProducts.map((item, index) => (
               <ProductCard
                 key={index}
                 img01={item.image_url}
@@ -49,7 +88,7 @@ const Product = (props) => {
                 slug={item.id}
               />
             ))}
-          </Grid> */}
+          </Grid>
         </SectionBody>
       </Section>
     </Helmet>
